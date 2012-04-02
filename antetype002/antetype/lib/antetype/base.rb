37e23731ac06a37837274c8747179ecbde1243d2
Gudao Luo <gudao.luo@gmail.com>
2012-04-02 22:09:21 +0200

    projects antetype002 uncomplete

--- antetype002/antetype/lib/antetype/base.rb
+++ antetype002/antetype/lib/antetype/base.rb
@@ -2,7 +2,7 @@ require 'thor'
 
 module Antetype
   class Base  < Thor
-  	desc "show_antetype_dir", "show antetype dir"
+    desc "show_antetype_dir", "show antetype dir"
     def show_antetype_dir
       say "#{Antetype::DEFAULT_CONFIG_PATH}", :green
     end
